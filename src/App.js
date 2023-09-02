2c1176625f63f7ae551a06930d67fd94025a445f
BangaruKumarChintha <bangarukumar1997@gmail.com>
2023-09-02 12:27:14 +0530

    commit
    Date: 02-09-2023

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navbar/>
+        <Main/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
